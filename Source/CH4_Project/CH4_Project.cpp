00b59d3d0021f7e1585c58031989041f0a3a1844
heewonle <lhw4540@naver.com>
2026-04-01 14:15:32 +0900

    [이희원] 기본 프로젝트 생성/맵,캐릭터 관련 에셋 추가

---
+++ Source/CH4_Project/CH4_Project.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CH4_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CH4_Project, "CH4_Project" );
