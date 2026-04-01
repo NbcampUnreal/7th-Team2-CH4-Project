df0bcb9301aa89976dc2a8659856f19454dcbce6
NMEii <skaxo0823@gmail.com>
2026-04-02 01:54:50 +0900

    [김남태] GridSubsystem 기본 구현 및 테스트 레벨 추가
    GridSubsystem을 테스트 레벨에서 커서 위치 라인트레이스에 따라 DebugDraw 확인

---
+++ Source/CH4_Project/Subsystems/GridCellData.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GridCellData.generated.h"
+
+UENUM(BlueprintType)
+enum class ECellVisibilityState : uint8
+{
+	Hidden		UMETA(DisplayName = "Hidden"),
+	Explored	UMETA(DisplayName = "Explored"),
+	Visible		UMETA(DisplayName = "Visible")
+};
+
+USTRUCT(BlueprintType)
+struct FGridCellData
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Grid|Flags")
+	uint8 bIsBuildable : 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Grid|Flags")
+	uint8 bIsDestroyed : 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Grid|Flags")
+	uint8 bHasTrap : 1;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Grid|Construction")
+	TObjectPtr<AActor> OccupyingBuilding;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Grid|Construction")
+	ECellVisibilityState VisibilityState;
+	
+	FGridCellData()
+		: bIsBuildable(true)
+		, bIsDestroyed(false)
+		, bHasTrap(false)
+		, OccupyingBuilding(nullptr)
+		, VisibilityState(ECellVisibilityState::Hidden)
+	{}
+};
---
+++ Source/CH4_Project/Subsystems/GridSubSystem.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GridSubSystem.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+
+void UGridSubSystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	
+	int32 TotalCells = GridDimensions.X * GridDimensions.Y;
+	GridData.SetNum(TotalCells);
+	
+	UE_LOG(LogTemp, Warning, TEXT("TotalCells: %d"), TotalCells);
+}
+
+FIntPoint UGridSubSystem::WorldToGridPosition(const FVector& WorldLocation) const
+{
+	int32 X = FMath::FloorToInt(WorldLocation.X / CellSize);
+	int32 Y = FMath::FloorToInt(WorldLocation.Y / CellSize);
+	
+	return FIntPoint(X, Y);
+}
+
+FVector UGridSubSystem::GridToWorldPosition(const FIntPoint& GridLocation) const
+{
+	float WorldX = (GridLocation.X * CellSize) + (CellSize * 0.5f);
+	float WorldY = (GridLocation.Y * CellSize) + (CellSize * 0.5f);
+	
+	return FVector(WorldX, WorldY, 0.0f);
+}
+
+bool UGridSubSystem::IsValidGridLocation(const FIntPoint& GridLocation) const
+{
+	return	GridLocation.X >= 0 && GridLocation.X < GridDimensions.X &&
+			GridLocation.Y >= 0 && GridLocation.Y < GridDimensions.Y;
+}
