1e14fa8bb1ae8dde8eccea4b0e4a4b0eb7f36ca1
NMEii <skaxo0823@gmail.com>
2026-04-08 14:07:10 +0900

    [김남태] 건설 기능 testblueprint

---
+++ Source/CH4_Project/Subsystems/TWGridSubSystem.cpp
@@ -0,0 +1,169 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TWGridSubSystem.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+
+void UTWGridSubSystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	
+	int32 TotalCells = GridDimensions.X * GridDimensions.Y;
+	GridData.SetNum(TotalCells);
+	
+	UE_LOG(LogTemp, Warning, TEXT("TotalCells: %d"), TotalCells);
+}
+
+void UTWGridSubSystem::OnWorldBeginPlay(UWorld& InWorld)
+{
+	Super::OnWorldBeginPlay(InWorld);
+	
+	FCollisionQueryParams QueryParams;	
+	QueryParams.bTraceComplex = true;
+	
+	ECollisionChannel LandscapeChannel = ECC_GameTraceChannel1;
+	
+	for (int32 Y = 0; Y < GridDimensions.Y; Y++)
+	{
+		for (int32 X = 0; X < GridDimensions.X; X++)
+		{
+			float WorldX = (X * CellSize) + (CellSize * 0.5f);// + GridOrigin.X;
+			float WorldY = (Y * CellSize) + (CellSize * 0.5f);// + GridOrigin.Y;
+			
+			FVector TraceStart(WorldX, WorldY, 10000.0f);
+			FVector TraceEnd(WorldX, WorldY, -10000.0f);
+			FHitResult HitResult;
+			
+			bool bHit = InWorld.LineTraceSingleByChannel(
+					HitResult
+					, TraceStart
+					, TraceEnd
+					, LandscapeChannel
+					, QueryParams
+			);
+			
+			int32 Index = (Y * GridDimensions.X) + X;
+			
+			if (bHit)
+			{
+				GridData[Index].TerrarianZ = HitResult.ImpactPoint.Z;
+				
+				DrawDebugPoint(&InWorld, HitResult.ImpactPoint, 10.0f, FColor::Green, true);
+			}
+			else
+			{
+				GridData[Index].TerrarianZ = 0.0f;
+				GridData[Index].bIsBuildable = false;
+				
+				DrawDebugLine(&InWorld, TraceStart, TraceEnd, FColor::Red, true);
+			}
+		}
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Z축 캐싱 완료"));
+}
+
+FIntPoint UTWGridSubSystem::WorldToGridPosition(const FVector& WorldLocation) const
+{
+	int32 X = FMath::FloorToInt(WorldLocation.X / CellSize);
+	int32 Y = FMath::FloorToInt(WorldLocation.Y / CellSize);
+	
+	return FIntPoint(X, Y);
+}
+
+FVector UTWGridSubSystem::GridToWorldPosition(const FIntPoint& GridLocation) const
+{
+	float WorldX = (GridLocation.X * CellSize) + (CellSize * 0.5f);// + GridOrigin.X;
+	float WorldY = (GridLocation.Y * CellSize) + (CellSize * 0.5f);// + GridOrigin.Y;
+	float CachedZ = 0.0f;
+	
+	if (IsValidGridLocation(GridLocation))
+	{
+		int32 Index = (GridLocation.Y * GridDimensions.X)+GridLocation.X;
+		CachedZ = GridData[Index].TerrarianZ;
+	}
+	
+	return FVector(WorldX, WorldY, CachedZ);
+}
+
+bool UTWGridSubSystem::CanBuildArea(FIntPoint AnchorLocation, FIntPoint BuildingSize) const
+{
+	for (int32 Y = 0; Y < BuildingSize.Y; Y++)
+	{
+		for (int32 X = 0; X < BuildingSize.X; X++)
+		{
+			FIntPoint TargetGrid(AnchorLocation.X + X, AnchorLocation.Y + Y);
+			
+			if (!CanBuildAt(TargetGrid))
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+void UTWGridSubSystem::OccupyArea(FIntPoint AnchorLocation, FIntPoint BuildingSize, AActor* BuildingAnchor)
+{
+	for (int32 Y = 0; Y < BuildingSize.Y; ++Y)
+	{
+		for (int32 X = 0; X < BuildingSize.X; ++X)
+		{
+			FIntPoint TargetGrid(AnchorLocation.X + X, AnchorLocation.Y + Y);
+			
+			if (!CanBuildAt(TargetGrid))
+			{
+				PlaceBuildingAt(TargetGrid, BuildingAnchor);
+			}
+		}
+	}
+}
+
+FVector UTWGridSubSystem::GetBuildingCenterPosition(FIntPoint AnchorLocation, FIntPoint BuildingSize) const
+{
+	float WorldX = (AnchorLocation.X * CellSize) + (BuildingSize.X * CellSize *0.5f);// + GridOrigin.X;
+	float WorldY = (AnchorLocation.Y * CellSize) + (BuildingSize.Y * CellSize *0.5f);// + GridOrigin.Y;
+	float CachedZ = 0.0f;
+	
+	if (IsValidGridLocation(AnchorLocation))
+	{
+		int32 Index = (AnchorLocation.Y * GridDimensions.X) + AnchorLocation.X;
+		CachedZ = GridData[Index].TerrarianZ;
+	}
+	
+	return FVector(WorldX, WorldY, CachedZ);
+}
+
+bool UTWGridSubSystem::CanBuildAt(const FIntPoint& GridLocation) const
+{
+	if (!IsValidGridLocation(GridLocation))
+	{
+		return false;
+	}
+	
+	int32 Index = (GridLocation.Y * GridDimensions.X) + GridLocation.X;
+	
+	if (!GridData[Index].bIsBuildable || GridData[Index].OccupyingBuilding != nullptr)
+	{
+		return false;
+	}
+	
+	return true;
+}
+
+void UTWGridSubSystem::PlaceBuildingAt(const FIntPoint& GridLocation, AActor* Building)
+{
+	if (IsValidGridLocation(GridLocation))
+	{
+		int32 Index = (GridLocation.Y * GridDimensions.X) + GridLocation.X;
+		
+		GridData[Index].OccupyingBuilding = Building;
+		GridData[Index].bIsBuildable = false;
+	}
+}
+
+bool UTWGridSubSystem::IsValidGridLocation(const FIntPoint& GridLocation) const
+{
+	return	GridLocation.X >= 0 && GridLocation.X < GridDimensions.X &&
+			GridLocation.Y >= 0 && GridLocation.Y < GridDimensions.Y;
+}
---
+++ Source/CH4_Project/Subsystems/TWGridSubSystem.h
@@ -0,0 +1,67 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "GridCellData.h"
+#include "TWGridSubSystem.generated.h"
+
+
+UCLASS()
+class CH4_PROJECT_API UTWGridSubSystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void OnWorldBeginPlay(UWorld& InWorld) override;
+	
+#pragma region Grid
+	
+	UFUNCTION(BlueprintCallable, Category = "Grid|Coordinate")
+	FIntPoint WorldToGridPosition(const FVector& WorldLocation) const;	// 월드 3D 좌표 -> 논리 2D 그리드 좌표
+	UFUNCTION(BlueprintCallable, Category = "Grid|Coordinate")
+	FVector GridToWorldPosition(const FIntPoint& GridLocation) const;	// 논리 2D 그리드 좌표 -> 월드 3D 좌표
+	
+#pragma endregion
+	
+#pragma region Util
+	
+	UFUNCTION(BlueprintPure, Category = "Grid|Settings")
+	float GetCellSize() const { return CellSize; }
+	UFUNCTION(BlueprintPure, Category = "Grid|Settings")
+	FIntPoint GetGridDimensions() const { return GridDimensions; }
+	
+#pragma endregion 
+	
+	UFUNCTION(BlueprintCallable, Category = "Grid|Construction")
+	bool CanBuildArea(FIntPoint AnchorLocation, FIntPoint BuildingSize) const;
+	
+	UFUNCTION(BlueprintCallable, Category = "Grid|Construction")
+	void OccupyArea(FIntPoint AnchorLocation, FIntPoint BuildingSize, AActor* BuildingAnchor);
+	
+	UFUNCTION(BlueprintCallable, Category = "Grid|Coordinate")
+	FVector GetBuildingCenterPosition(FIntPoint AnchorLocation, FIntPoint BuildingSize) const;
+	
+	UFUNCTION(BlueprintCallable, Category = "Grid|Construction")
+	bool CanBuildAt(const FIntPoint& GridLocation) const;	// 좌표에 건물을 건설할 수 있는지 검사
+	UFUNCTION(BlueprintCallable, Category = "Grid|Construction")
+	void PlaceBuildingAt(const FIntPoint& GridLocation, AActor* Building);	// 좌표에 건물을 건설하고 데이터 업데이트
+	
+	// UFUNCTION(BlueprintCallable, Category = "Grid|FogOfWar")
+	// void UpdateVisionAroundLocation(const FIntPoint& CenterLocation, int32 VisionRadius);	// 건물 주변 반경 시야 Visible로 갱신
+	//
+	
+private:
+	UPROPERTY(EditDefaultsOnly, Category="Grid|Settings")
+	float CellSize = 100.0f;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Grid|Settings")
+	FIntPoint GridDimensions = FIntPoint(200, 200);
+	
+	UPROPERTY(EditDefaultsOnly, Category="Grid|Settings")
+	FVector GridOrigin = FVector(-3100.0f, -3100.0f, 0.0f); // 그리드 기준점 이동 변수 / 현재 테스트 레벨 사용중이라 미사용
+	
+	TArray<FGridCellData> GridData;	
+	
+	bool IsValidGridLocation(const FIntPoint& GridLocation) const;	// 그리드 좌표 유효 범위 확인
+};
