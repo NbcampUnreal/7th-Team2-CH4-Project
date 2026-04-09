67da48e2dd09c3be461a77a9424e41ec8fc74bdd
shy3939 <kpky33@naver.com>
2026-04-09 20:57:10 +0900

    [유시환] 명령-정지
    Fragment기초 작업은 완

---
+++ Source/CH4_Project/Public/Mass/Fragments/TWStandFragment.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "MassEntityElementTypes.h"
+#include "TWStandFragment.generated.h"
+
+USTRUCT()
+struct FTWStopMovementTag : public FMassTag
+{
+	GENERATED_BODY()
+};
+
+USTRUCT()
+struct FTWStandFragment : public FMassFragment
+{
+	GENERATED_BODY()
+};
