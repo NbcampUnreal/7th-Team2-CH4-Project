2fff82b7161e194dc8fd92b0d03d89b4e12a28e2
NMEii <skaxo0823@gmail.com>
2026-04-09 11:51:06 +0900

    [김남태] 건설기능 테스트

---
+++ Source/CH4_Project/Public/Building/GhostBuilding.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GhostBuilding.generated.h"
+
+UCLASS()
+class CH4_PROJECT_API AGhostBuilding : public AActor
+{
+	GENERATED_BODY()
+
+public:
+
+	AGhostBuilding();
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+	
+	void SetBuildingMesh(UStaticMesh* Mesh, FIntPoint BuildingSize);
+	void UpdateBuildingVisual(bool bCanBuild);
+	
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> StaticMesh;
+	
+	UPROPERTY()
+	TObjectPtr<UMaterialInstanceDynamic> BuildingMID;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UMaterialInterface> BaseBuildingMaterial;
+	
+};
--- Source/CH4_Project/Public/Component/TWBuildComponent.h
+++ Source/CH4_Project/Public/Component/TWBuildComponent.h
@@ -1,6 +1,4 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
@@ -13,15 +11,14 @@ class CH4_PROJECT_API UTWBuildComponent : public UActorComponent
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this component's properties
 	UTWBuildComponent();
 
 protected:
-	// Called when the game starts
+
 	virtual void BeginPlay() override;
 
 public:
-	// Called every frame
+
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
 	                           FActorComponentTickFunction* ThisTickFunction) override;
 };
