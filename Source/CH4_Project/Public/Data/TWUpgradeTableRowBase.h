0151abc3acde9ddb8ed4305b514e9478f7c901a0
jcmeve <jcmeve@naver.com>
2026-04-10 14:58:52 +0900

    [조창민] 유닛, 업그레이드 DT, 스탯 개편

---
+++ Source/CH4_Project/Public/Data/TWUpgradeTableRowBase.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MassEntityConfigAsset.h"
+#include "TWBuildingTypes.h"
+#include "TWUnitStatus.h"
+#include "TWUpgradeTableRowBase.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct  CH4_PROJECT_API FTWUpgradeTableRowBase : public FTableRowBase
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Upgrade")
+	FName UpgradeID;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Upgrade")
+	ETWStatusType TargetStatus = ETWStatusType::Damage;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Upgrade")
+	TMap<FName, int32> TargetUnits;//적용대상 유닛 ID, 적용수치
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Upgrade")
+	TMap<EResourceType, int32> Cost;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Upgrade")
+	TMap<EResourceType, int32> CostIncreaseAmount;//업그레이드 할 떄 마다 비싸지는 비용
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Upgrade")
+	float Duration = 1;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Upgrade")
+	TObjectPtr<UTexture2D> Icon;	
+
+};
