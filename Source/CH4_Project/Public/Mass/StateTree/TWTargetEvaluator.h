bc52e27d16d819fd859488355db7472853ff0d75
shy3939 <kpky33@naver.com>
2026-04-13 12:46:50 +0900

    [유시환] 타겟 서칭 Evaluator

---
+++ Source/CH4_Project/Public/Mass/StateTree/TWTargetEvaluator.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "MassStateTreeTypes.h"
+#include "TWTargetEvaluator.generated.h"
+
+struct FTWAttackFragment;
+struct FTWTargetFragment;
+
+USTRUCT()
+struct FTWTargetEvaluatorInstanceData
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere, Category = "Target")
+	bool bIsEnemyInRange;
+	
+};
+
+USTRUCT()
+struct FTWTargetEvaluator : public FMassStateTreeEvaluatorBase
+{
+	GENERATED_BODY()
+	using FInstanceDataType = FTWTargetEvaluatorInstanceData;
+protected:
+	TStateTreeExternalDataHandle<FTWAttackFragment>AttackHandle;
+	TStateTreeExternalDataHandle<FTWTargetFragment>TargetHandle;
+	
+	virtual bool Link(FStateTreeLinker& Linker) override;
+	virtual void Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const override;
+};
