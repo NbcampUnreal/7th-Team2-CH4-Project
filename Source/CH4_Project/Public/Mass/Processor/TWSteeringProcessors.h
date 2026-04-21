088549203cbc0838ace6aec10a588fd99fd56c63
jcmeve <jcmeve@naver.com>
2026-04-21 21:24:33 +0900

    [조창민] Custom Steering Base

---
+++ Source/CH4_Project/Public/Mass/Processor/TWSteeringProcessors.h
@@ -0,0 +1,26 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "MassProcessor.h"
+#if UE_ENABLE_INCLUDE_ORDER_DEPRECATED_IN_5_6
+#include "MassObserverProcessor.h"
+#endif // UE_ENABLE_INCLUDE_ORDER_DEPRECATED_IN_5_6
+#include "TWSteeringProcessors.generated.h"
+
+/** 
+* Processor for updating steering towards MoveTarget.
+*/
+UCLASS()
+class UTWSteerToMoveTargetProcessor : public UMassProcessor
+{
+	GENERATED_BODY()
+
+protected:
+	UTWSteerToMoveTargetProcessor();
+	
+	virtual void ConfigureQueries(const TSharedRef<FMassEntityManager>& EntityManager) override;
+	virtual void Execute(FMassEntityManager& EntityManager, FMassExecutionContext& Context) override;
+
+	FMassEntityQuery EntityQuery;
+};
---
+++ Source/CH4_Project/Public/Mass/Traits/TWSteeringTrait.h
@@ -0,0 +1,27 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once 
+
+#include "MassEntityTraitBase.h"
+#include "Mass/Fragments/TWSteeringFragments.h"
+#include "TWSteeringTrait.generated.h"
+
+#define UE_API MASSNAVIGATION_API
+
+
+UCLASS(meta = (DisplayName = "TWSteering"))
+class UTWSteeringTrait : public UMassEntityTraitBase
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BuildTemplate(FMassEntityTemplateBuildContext& BuildContext, const UWorld& World) const override;
+
+	UPROPERTY(Category="Steering", EditAnywhere, meta=(EditInline))
+	FTWMovingSteeringParameters MovingSteering;
+
+	UPROPERTY(Category="Steering", EditAnywhere, meta=(EditInline))
+	FTWStandingSteeringParameters StandingSteering;
+};
+
+#undef UE_API
