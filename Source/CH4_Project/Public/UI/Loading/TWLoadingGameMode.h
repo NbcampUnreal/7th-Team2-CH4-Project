8487bafd5bb1a589b401dd97c7adfeb2e26372d2
shy3939 <kpky33@naver.com>
2026-04-22 16:24:01 +0900

    [유시환] 로딩 화면 적용

---
+++ Source/CH4_Project/Public/UI/Loading/TWLoadingGameMode.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "TWLoadingGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CH4_PROJECT_API ATWLoadingGameMode : public AGameMode
+{
+	GENERATED_BODY()
+};
